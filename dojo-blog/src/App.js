8ca99160058d28e6826972e0b53eace08947601e
hnguye24 <nguyenhenry1996@gmail.com>
2023-03-15 17:09:52 -0500

    multiple components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 // this is a component
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "https://www.google.com";
-
   return (
     // JSX template
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10}</p>
-
-        <a href={ link }>Google</a>
+        <Home />
       </div>
     </div>
   );
