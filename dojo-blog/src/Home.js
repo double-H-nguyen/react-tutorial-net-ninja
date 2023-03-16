9b008b641dbd05b0985c0b2e6548d6cb3f32f1ed
hnguye24 <nguyenhenry1996@gmail.com>
2023-03-16 14:43:27 -0500

    click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,6 +1,22 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas')
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
-        <h2>Home Page</h2>
+        <div className="home">
+            <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+
+            {/* Use anonymous function to use value in function with instantiating it */}
+            {/* reference event */}
+            <button onClick={(e) => handleClickAgain('henry', e)}>Click me again</button>
+        </div>
     );
 }
  
